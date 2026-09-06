b00937a8f1db23e83aa3e612796cf949fe4afef6
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-06 20:02:38 +0530

    Added Bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+    // Bubble Sort -> O(n2)
+    public static void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // swap...
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // Print function..
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 1, 2};
+
+        sort(arr);
+        printArray(arr);
+    }
+}
