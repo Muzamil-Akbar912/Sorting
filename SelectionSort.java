2ff05203b538a4d9e7c4c733bb88fc1e0c241505
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-06 21:15:21 +0530

    Added Selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int minPos = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // Swap..
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    // Print function..
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 1, 2};
+
+        sort(arr);
+        printArray(arr);
+    }
+}
